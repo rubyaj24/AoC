685625a1642ea13cebf1a21bae28a97694585499
AMALJITH <149690738+rubyaj24@users.noreply.github.com>
2024-12-03 13:54:08 +0530

    Update 2.c

--- 2024/2.c
+++ 2024/2.c
@@ -4,11 +4,12 @@
 
 int checkdifference(int arr[], int size){
     for(int i = 0; i < size-1; i++){
-        if(arr[i]-arr[i+1] <= 0 && arr[i]-arr[i+1] > 3){
+        int diff = abs(arr[i] - arr[i+1]);
+        if(diff < 1 || diff > 3){
             return 0;
         }
-    return 1;
     }
+    return 1;
 }
 
 int main(){
@@ -21,17 +22,17 @@ int main(){
     int arr[10];
     int size = 0;
     int safe = 0;
-        while(fscanf(file,"%d",&arr[size]) == 1){
-            size++;
-            int ch = getc(file);
-            if (ch == '\n' || ch == EOF){
-                safe += checkdifference(arr, size);
-                size = 0;
-            }
-            if(ch == EOF){
-                break;
-            }
+    while(fscanf(file,"%d",&arr[size]) == 1){
+        size++;
+        int ch = getc(file);
+        if (ch == '\n' || ch == EOF){
+            safe += checkdifference(arr, size);
+            size = 0;
         }
+        if(ch == EOF){
+            break;
+        }
+    }
     printf("Safe: %d\n",safe);
     fclose(file);
     return 0;
